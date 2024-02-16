7f7b30d7f1a5c85020fed0f94d427b9b97d34a9b
Afifpip <afif.firfik@gmail.com>
2024-02-16 23:35:56 +0700

    animal

---
+++ python-flask-revou/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from app.route import animal_route
+import os
+from app.utils.database import db
+
+app = Flask(__name__)
+
+DATABASE_TYPE = os.getenv('DATABASE_TYPE')
+DATABASE_USER = os.getenv('DATABASE_USER')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+DATABASE_PORT = os.getenv('DATABASE_PORT')
+app.config["SQLALCHEMY_DATABASE_URL"] = f"{DATABASE_TYPE}://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+db.init_app(app)
+
+app.register_blueprint(animal_route.animal_blueprint, url_prefix="/animal")
+
+# @app.route('/')
+# def my_app():
+#   return 'helloasdas'
